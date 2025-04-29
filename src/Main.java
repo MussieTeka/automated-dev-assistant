6866ad15414fb1507a7c33c5e5ba96da4ae0620b
Mussie Teka <mussiekahasay12@gmail.com>
2025-04-29 13:55:55 -0500

    Intial commit: Project setup

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Automated Development Assistant!");
+        // Logic will go here
+    }
+}
